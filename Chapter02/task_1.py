c0edb2f0ac070d74e79544e649ac2b0e470b2b82
Jay M Gonzalez <jaimungon@gmail.com>
2021-03-15 11:39:43 +0100

    [ADD] contrab script

--- Chapter02/task_1.py
+++ Chapter02/task_1.py
@@ -1,21 +1,22 @@
 import argparse
 import configparser
 import sys
+from datetime import datetime
 
 
 def main(number, other_number, output):
     result = number * other_number
-    print(f'The result is {result}', file=output)
+    print(f'[{datetime.utcnow().isoformat()}] The result is {result}', file=output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='multiply the 2 numbers provided by config file or arguments and save it into a file')
-    parser.add_argument('-n1', type=int, help='A number', default=1)
-    parser.add_argument('-n2', type=int, help='Another number', default=1)
+        description='''multiply the 2 numbers provided by config file or arguments and save it into 
+        a file and show the time when was executed''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     parser.add_argument(
-        '--config', '-c', type=argparse.FileType('r'), help='config file')
-    parser.add_argument('-o', dest='output', type=argparse.FileType('w'),
+        '--config', '-c', type=argparse.FileType('r'), help='config file', default='/home/jmunoz/projects/python/automation/Python-Automation-Cookbook-Second-Edition/Chapter02/config.ini')
+    parser.add_argument('-o', dest='output', type=argparse.FileType('a'),
                         help='output file', default=sys.stdout)
 
     args = parser.parse_args()
