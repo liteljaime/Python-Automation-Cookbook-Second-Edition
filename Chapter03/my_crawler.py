431f51e1c8afd2193c32b2a32b34cb4e8c19920b
Jay M Gonzalez <jaimungon@gmail.com>
2021-03-19 16:15:03 +0100

    investigation of crawling script

--- Chapter03/my_crawler.py
+++ Chapter03/my_crawler.py
@@ -13,15 +13,7 @@ DEFAULT_PHRASE = 'python'
 def process_link(source_link, text):
     logging.info(f'Extracting links from {source_link}')
     parsed_source = urlparse(source_link)
-    # print('parsed source ->', parsed_source)
     result = requests.get(source_link)
-    if result.status_code != http.client.OK:
-        logging.error(f'Error retrieving {source_link}: {result}')
-        return []
-
-    if 'html' not in result.headers['Content-type']:
-        logging.info(f'Link {source_link} is not an HTML page')
-        return []
 
     page = BeautifulSoup(result.text, 'html.parser')
     search_text(source_link, page, text)
@@ -33,12 +25,9 @@ def get_links(parsed_source, page):
     '''Retrieve the links on the page'''
     links = []
     for element in page.find_all('a'):
-        # print(element)
         link = element.get('href')
-        # print(link)
         if not link:
             continue
-
         # Avoid internal, same page links
         if link.startswith('#'):
             continue
@@ -50,12 +39,10 @@ def get_links(parsed_source, page):
 
         # Always accept local links
         if not link.startswith('http'):
-            # print(link)
             netloc = parsed_source.netloc
             scheme = parsed_source.scheme
-            path = urljoin(parsed_source.path, link)
+            path = parsed_source.path
             link = f'{scheme}://{netloc}{path}'
-            # print(link)
 
         # Only parse links in the same domain
         if parsed_source.netloc not in link:
@@ -69,42 +56,5 @@ def get_links(parsed_source, page):
 def search_text(source_link, page, text):
     '''Search for an element with the searched text and print it'''
     for element in page.find_all(text=re.compile(text, flags=re.IGNORECASE)):
-        # print(element)
         print(
             f'This link {source_link}: --> has the following text: {element}')
-
-
-def main(base_url, to_search):
-    checked_links = set()
-    to_check = [base_url]
-    max_checks = 10
-    # print(to_check)
-
-    while to_check and max_checks:
-        link = to_check.pop(0)
-        print(to_check)
-        links = process_link(link, text=to_search)
-        print(links)
-        checked_links.add(link)
-        for link in links:
-            # print(link)
-            if link not in checked_links:
-                checked_links.add(link)
-                to_check.append(link)
-
-        max_checks -= 1
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(dest='url', type=str,
-                        help='Base site url. '
-                        'Use  "http://localhost:8000/" '
-                        'for the recipe example',
-                        default='http://localhost:8000/')
-    parser.add_argument('-p', type=str,
-                        help=f'Sentence to search, default: {DEFAULT_PHRASE}',
-                        default=DEFAULT_PHRASE)
-    args = parser.parse_args()
-
-    main(args.url, args.p)
